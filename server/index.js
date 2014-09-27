52f71f346bf6cbbc0593f22dc6adfe57eddabef7
Hektor Wallin <hektorw@gmail.com>
2014-09-27 16:35:06 +0200

    some more basic setup, module system for shared modules

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@
 var express = require('express');
 // var io = require('socket.io');
 
+var BaseCircle = require('../shared/BaseCircle.js');
+new BaseCircle();
+
 var app = express();
 
 
 // server static files from client
 app.use(express.static('../client'));
+app.use('/shared', express.static('../shared'));
 
 app.listen(8080);
